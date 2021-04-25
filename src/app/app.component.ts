5694f808df81ce44a83d636fdb4ea22464c1785f
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2021-04-25 03:30:53 +0200

    first test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,176 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { environment } from 'src/environments/environment';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'markplats';
+export class AppComponent implements OnInit {
+  links: any[] = [];
+  displayedColumns: string[] = ['link', 'createdAt', 'delete'];
+  dataSource;
+  expandedElement: any;
+  @ViewChild(MatSort,{static:false}) sort: MatSort;
+  @ViewChild(MatPaginator,{static:false}) paginator: MatPaginator;
+
+  constructor(private httpClient: HttpClient){
+
+  }
+  ngOnInit(): void {
+    this.getAllLinks();
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  displayToast(msg:string,loading:boolean,type:string) {
+    const Toast = Swal.mixin({
+      toast: true,
+      target: '#custom-target',
+      position: 'bottom-start',
+      showConfirmButton: false,
+      className: "pos-toast-swt",
+      background:this.handleSnackBar(type),
+      didOpen: (toast) => {
+        if (loading) Swal.showLoading();
+
+      }
+    })
+
+
+
+
+    Toast.fire({
+      icon: type,
+      title: msg
+    })
+    if (!loading) {
+      setTimeout(() => {
+        Toast.close()
+      },4000);
+    }
+
+  }
+
+  private handleSnackBar(type: string): string {
+    if (type == 'error'){
+      return '#d9534f';
+    }
+    else if (type == 'success'){
+      return '#5cb85c';
+    }
+    else if (type == 'warning'){
+      return '#f0ad4e';
+    }
+    else if (type == 'info'){
+      return '';
+    }
+    else {
+      return '';
+    }
+  }
+
+  async createLoading(msg?) {
+    Swal.fire({
+      title: 'Loading !',
+      html: msg,// add html attribute if you want or remove
+      allowOutsideClick: false,
+      showConfirmButton: false,
+      backdrop: false,
+      allowEscapeKey: false,
+      onBeforeOpen: () => {
+          Swal.showLoading()
+      },
+  });
+}
+
+createInputSwal() {
+  Swal.fire({
+    title: 'Update Link',
+    input: 'text',
+    inputAttributes: {
+      autocapitalize: 'off',
+      placeHolder:'ID OF SHEET'
+    },
+    showCancelButton: true,
+    confirmButtonText: 'Update',
+    showLoaderOnConfirm: true,
+    timer: 1500000,
+    preConfirm: (update) => {
+      return this.addLink(update)
+        .then(() => {
+
+        })
+        .catch(() => {
+
+        })
+    },
+    allowOutsideClick: () => !Swal.isLoading()
+  }).then((result) => {
+    console.log(result);
+    if (result.isConfirmed) {
+      Swal.fire({
+        title:"LINK UPDATED",
+        icon:'success',
+      })
+    }
+    else if (result.isDismissed){
+    }
+  })
+
+}
+
+getAllLinks() {
+  this.createLoading('Getting All Links Please Wait');
+  this.httpClient.get(environment.url + 'markplats/getlinks')
+    .subscribe((result:any) => {
+      Swal.close();
+      if (result && result.status == 200) {
+        this.displayToast('Links Loaded',false,'success');
+      }
+      else if (result && result.status == 404) {
+        this.displayToast('You have no links',false,'warning');
+      }
+      else {
+        this.displayToast('SOMETHING WENT WRONG',false,'error');
+
+      }
+    },err => {
+      Swal.close();
+      this.displayToast('SOMETHING WENT WRONG',false,'error');
+
+    })
+}
+
+addLink(link:String){
+  return new Promise((resolve,reject) => {
+    this.httpClient.post(environment.url + 'markplats/addlink',{link:link})
+    .subscribe((result:any) => {
+      if (result && result.status == 200) {
+        this.displayToast('Link Created Succesfully',false,'success');
+        resolve({status:true});
+      }
+      else {
+        Swal.showValidationMessage(
+          `Request failed: Something Went Wrong`
+        )
+      }
+    },err => {
+      Swal.showValidationMessage(
+        `Request failed: Something Went Wrong`
+      )
+    })
+
+  })
+
+
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,49 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule} from '@angular/material/table'
+import { MatSortModule } from "@angular/material/sort";
+import {  MatPaginatorModule } from '@angular/material/paginator';
+import {  MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule} from '@angular/material/input'
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatTooltipModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatSliderModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
